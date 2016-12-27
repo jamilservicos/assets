8b53f22d6d8a9ad8b2325c39719b75b7faec6cc0
Jamil Serviços <jamilservicos@gmail.com>
2016-12-27 15:50:04 -0300

    update

--- isil/js/isil.js
+++ isil/js/isil.js
@@ -87,7 +87,7 @@ function tString(t) {
 function loading() {
     document.getElementById("loading").remove();
     var loadingImage = new Image;
-    loadingImage.src = 'https://dl.dropboxusercontent.com/u/14626206/isil/assets/img/loadingSprite.png';
+    loadingImage.src = 'https://cdn.sr-1.tk/img/loadingSprite.png';
     loadingImage.onload = function (event) {
         var dataSpritesLoading = {
             "images": [loadingImage],
@@ -151,9 +151,9 @@ function loading() {
         stage.update();
         //
         manifest = [
-            {src: "https://dl.dropboxusercontent.com/u/14626206/isil/assets/img/background-loading.jpg", id: "background-loading"},
-            {src: "https://dl.dropboxusercontent.com/u/14626206/isil/assets/img/background-login.jpg", id: "background-login"},
-            {src: "https://dl.dropboxusercontent.com/u/14626206/isil/assets/css/login.css", id: "css-login"},
+            {src: "https://cdn.sr-1.tk/img/background-loading.jpg", id: "background-loading"},
+            {src: "https://cdn.sr-1.tk/img/background-login.jpg", id: "background-login"},
+            {src: "https://cdn.sr-1.tk/css/login.css", id: "css-login"},
             {src: "https://cdn.socket.io/socket.io-1.4.5.js", id: "socket.io"},
             {src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js", id: "jquery"}
         ];
